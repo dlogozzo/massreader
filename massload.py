a497e0ffa5a9034c071dae3a3ea482c26c8d7dd2
dlogozzo <dologozzo@gmail.com>
2017-03-15 13:06:09 -0400

    Update massload.py

--- massload.py
+++ massload.py
@@ -16,19 +16,19 @@ logging.basicConfig(level=logging.INFO,
                     filemode='w')
                     
 class Inputs(object):
-  """
-  Input df objects to mass merge
-  """
-  def __init__(self, separator=',', skiprows=None, skipfooter=None, path):
+	"""
+	Input df objects to mass merge
+	"""
+	def __init__(self, separator=',', skiprows=None, skipfooter=None, path):
 		self.path = path
 		self.skipfooter = skipfooter
 		self.separator = separator							      #comma separator default 
 		self.skiprows = skiprows
 		self.col_headers = []
 		self.col_headers = self.header_getter(self.path)     #grabs headers once to speed up merging
-    
+
 	def header_getter(self, path):
-		"""Grabs a random file from the provided path and gets the column headers"""
+	"""Grabs a random file from the provided path and gets the column headers"""
 
 		random_file = random.choice(os.listdir(path))
 		with open(self.path + "/"+ random_file, 'r') as fh:
@@ -42,15 +42,15 @@ class Inputs(object):
 					break
 		logging.info("Column Headers: %s ", str(self.col_headers))
 		return self.col_headers
-    
-    
+
+
 	def massread(self):
-  """Reads path and merges all files into one dataframe"""
-    allfiles = glob.glob(self.path + '/*.*')
-		df = [pd.read_csv(file_, sep = self.separator, skiprows = self.skiprows, skipfooter = self.skipfooter, usecols=self.col_headers, engine = 'python') for file_ in allfiles]
-		frame = pd.concat(df)
-    logging.info("----------Done with Massreading----------")
-    return frame
+	        """Reads path and merges all files into one dataframe"""
+	    	allfiles = glob.glob(self.path + '/*.*')
+	    	df = [pd.read_csv(file_, sep = self.separator, skiprows = self.skiprows, skipfooter = self.skipfooter, usecols=self.col_headers, engine = 'python') for file_ in allfiles]
+	    	frame = pd.concat(df)
+	    	logging.info("----------Done with Massreading----------")
+	    	return frame
 
 
 #initialize the class and merge
@@ -60,5 +60,3 @@ merge = Inputs(',', skiprows=1, skipfooter=3, 'F:/Desktop/TestFiles')
 df = merge.massread()
 
 
-    
-    
